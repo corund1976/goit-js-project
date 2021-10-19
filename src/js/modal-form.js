2381832ad4af020f7e33112f3f84afde421502e9
Anri <maystrenko22a@gmail.com>
2021-10-19 21:01:32 +0300

    functional modal

--- src/js/modal-form.js
+++ src/js/modal-form.js
@@ -4,14 +4,26 @@ const refs = {
   modalNode: document.querySelector('.backdrop'),
 };
 
-// console.log('modalBtnCloseNode', refs.modalBtnCloseNode);
 refs.eventsCardsList.addEventListener('click', onEventClick);
+refs.eventsCardsList.addEventListener('keydown', onKeyPress);
 refs.modalBtnCloseNode.addEventListener('click', onModalClose);
+refs.modalNode.addEventListener('click', onBackdropClick);
+
 function onEventClick(e) {
   e.preventDefault();
-
   refs.modalNode.classList.toggle('is-hidden');
 }
 function onModalClose(e) {
   refs.modalNode.classList.toggle('is-hidden');
+}
+function onBackdropClick(e) {
+  if (e.target === refs.modalNode) {
+    console.log('Click on backdrop!');
+    refs.modalNode.classList.toggle('is-hidden');
+  }
+}
+function onKeyPress(e) {
+  if (e.code === 'Escape') {
+    refs.modalNode.classList.toggle('is-hidden');
+  }
 }
