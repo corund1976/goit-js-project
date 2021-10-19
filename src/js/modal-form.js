c51f6e44e82e212c3e530f0d9eb8755255cb73e5
Anri <maystrenko22a@gmail.com>
2021-10-19 20:28:47 +0300

    change in modal form

--- src/js/modal-form.js
+++ src/js/modal-form.js
@@ -1,3 +1,17 @@
 const refs = {
-  eventsCard: document.querySelector('.events-section .card__item'),
+  eventsCardsList: document.querySelector('.events-section .card'),
+  modalBtnCloseNode: document.querySelector('.backdrop .modal__closed'),
+  modalNode: document.querySelector('.backdrop'),
 };
+
+// console.log('modalBtnCloseNode', refs.modalBtnCloseNode);
+refs.eventsCardsList.addEventListener('click', onEventClick);
+refs.modalBtnCloseNode.addEventListener('click', onModalClose);
+function onEventClick(e) {
+  e.preventDefault();
+
+  refs.modalNode.classList.toggle('is-hidden');
+}
+function onModalClose(e) {
+  refs.modalNode.classList.toggle('is-hidden');
+}
