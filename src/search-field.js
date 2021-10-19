f9072b45830c80ebb695585090089168c12e4645
Anna Andreikiv <andreikiv.ann@gmail.com>
2021-10-20 00:50:56 +0300

    choose country done

--- src/search-field.js
+++ src/search-field.js
@@ -6,31 +6,35 @@ const searchByCountryEl = document.querySelector('#country-search');
 const DEBOUNCE_DELAY = 300;
 
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
-const API_KEY = 'Q9savgjBFnHGJj2EK0EB2bfGv110AECA';
+const API_KEY = 'kGyK62KCJILapDAPE9fz0caemViSYQAs';
+// 'Q9savgjBFnHGJj2EK0EB2bfGv110AECA';
 //  'kGyK62KCJILapDAPE9fz0caemViSYQAs';
 //https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Q9savgjBFnHGJj2EK0EB2bfGv110AECA
 // ФУНКЦИЯ ПОИСКА ПО ЗАПРОСУ
+
+let country;
 const search = async function () {
   // БЕРЁМ ДАННЫЕ ПОЛЬЗОВАТЕЛЯ
   const userQuery = searchFieldEl.value || '';
   // ОТПРАВЛЯЕМ ЗАПРОС НА СЕРВЕР (ДАННЫЕ ПОЛЬЗОВАТЕЛЯ В ПАРАМЕТРЕ)
-  sendServerRequest(userQuery);
-  const reply = await sendServerRequest(userQuery);
+  // sendServerRequest(userQuery, country);
+  const reply = await sendServerRequest(userQuery, country);
   //   ВЫЗЫВАЕМ ФУНКЦИЮ ОТРИСОВКИ РАЗМЕТКИ
   renderMarkup(reply);
 };
 
 // ФУНКЦИЯ ДЛЯ ЗАПРОСОВ НА СЕРВЕР
-const sendServerRequest = async function (userQuery) {
+const sendServerRequest = async function (userQuery, country = '') {
   let url;
   if (userQuery === '') {
-    url = `${BASE_URL}events.json?apikey=${API_KEY}`;
+    url = `${BASE_URL}events.json?countryCode=${country}&apikey=${API_KEY}`;
   } else {
-    url = `${BASE_URL}events.json?keyword=${userQuery}&apikey=${API_KEY}`;
+    url = `${BASE_URL}events.json?countryCode=${country}&keyword=${userQuery}&apikey=${API_KEY}`;
+    console.log(url);
   }
 
   const response = await fetch(url);
-  // console.log(response.status);
+
   if (response.status >= 200 && response.status < 300) {
     const events = await response.json();
     return events;
@@ -46,7 +50,6 @@ const renderMarkup = function (searchedEvents) {
     totalEl = searchedEvents.page.totalElements;
   }
   for (let i = 0; i < totalEl; i++) {
-    console.log(searchedEvents._embedded.events[i]);
     render += ` <li class="card__item">
         <a href="" class="card__item__link link">
             <div class="border-card"></div>
@@ -70,9 +73,11 @@ const renderMarkup = function (searchedEvents) {
 
 const showCountries = function () {
   renderListMarkup(config);
+  // country = searchByCountryEl.value;
+  // console.log(country);
 };
 // ФУНКЦИЯ ДЛЯ РЕНДЕРИНГА РАЗМЕТКИ ВЫПАДАЮЩЕГО СПИСКА
-
+console.log(country);
 const renderListMarkup = function (config) {
   searchByCountryEl.innerHTML = '';
   const array = Object.entries(config);
@@ -80,7 +85,12 @@ const renderListMarkup = function (config) {
   searchByCountryEl.insertAdjacentHTML('beforeend', markup);
 };
 searchFieldEl.addEventListener('input', debounce(search, DEBOUNCE_DELAY));
-searchByCountryEl.addEventListener('click', debounce(showCountries, DEBOUNCE_DELAY));
+// searchByCountryEl.addEventListener('click', debounce(showCountries, DEBOUNCE_DELAY));
+showCountries();
+searchByCountryEl.addEventListener('change', e => {
+  country = e.target.value;
+  search();
+});
 document.querySelector('form').addEventListener('submit', e => {
   e.preventDefault();
 });
