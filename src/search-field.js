bcf7480e2befe886308db743957dc940106e41a0
Aleksandr Kulyk <ale.kulyk@gmail.com>
2021-10-19 20:24:52 +0300

    часть изменений

--- src/search-field.js
+++ src/search-field.js
@@ -6,46 +6,35 @@ const searchByCountryEl = document.querySelector('#country-search');
 const DEBOUNCE_DELAY = 300;
 
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
-const API_KEY = 'Q9savgjBFnHGJj2EK0EB2bfGv110AECA';
-//  'kGyK62KCJILapDAPE9fz0caemViSYQAs';
-//https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Q9savgjBFnHGJj2EK0EB2bfGv110AECA
+const API_KEY = 'kGyK62KCJILapDAPE9fz0caemViSYQAs';
+let userQuery;
+let country;
 // ФУНКЦИЯ ПОИСКА ПО ЗАПРОСУ
 const search = async function () {
   // БЕРЁМ ДАННЫЕ ПОЛЬЗОВАТЕЛЯ
-  const userQuery = searchFieldEl.value || '';
+  const userQuery = searchFieldEl.value;
   // ОТПРАВЛЯЕМ ЗАПРОС НА СЕРВЕР (ДАННЫЕ ПОЛЬЗОВАТЕЛЯ В ПАРАМЕТРЕ)
-  sendServerRequest(userQuery);
-  const reply = await sendServerRequest(userQuery);
+  sendServerRequest(userQuery, country);
+  const reply = await sendServerRequest(userQuery,country);
   //   ВЫЗЫВАЕМ ФУНКЦИЮ ОТРИСОВКИ РАЗМЕТКИ
   renderMarkup(reply);
 };
 
 // ФУНКЦИЯ ДЛЯ ЗАПРОСОВ НА СЕРВЕР
-const sendServerRequest = async function (userQuery) {
-  let url;
-  if (userQuery === '') {
-    url = `${BASE_URL}events.json?apikey=${API_KEY}`;
-  } else {
-    url = `${BASE_URL}events.json?keyword=${userQuery}&apikey=${API_KEY}`;
-  }
+const sendServerRequest = async function (userQuery, country = '') {
+  let url = `${BASE_URL}events.json?keyword=${userQuery}&countryCode=${country}&apikey=${API_KEY}`;
+  console.log(url);
+  
   const response = await fetch(url);
-  console.log(response.status);
-  if (response.status >= 200 && response.status < 300) {
-    const events = await response.json();
-    return events;
-  } else return Promise.reject(console.log('Requst error'));
+  const events = await response.json();
+  console.log(events);
+  
+  return events;
 };
 
 const renderMarkup = function (searchedEvents) {
   let render = '';
-  let totalEl;
-  if (searchedEvents.page.totalElements > searchedEvents.page.size) {
-    totalEl = searchedEvents.page.size;
-  } else {
-    totalEl = searchedEvents.page.totalElements;
-  }
-  for (let i = 0; i < totalEl; i++) {
-    console.log(searchedEvents._embedded.events[i]);
+  for (let i = 0; i < searchedEvents.page.totalElements; i++) {
     render += ` <li class="card__item">
         <a href="" class="card__item__link link">
             <div class="border-card"></div>
@@ -68,6 +57,9 @@ const renderMarkup = function (searchedEvents) {
 };
 
 const showCountries = function () {
+    country = searchByCountryEl.value;
+    console.log(country);
+    
   renderListMarkup(config);
 };
 // ФУНКЦИЯ ДЛЯ РЕНДЕРИНГА РАЗМЕТКИ ВЫПАДАЮЩЕГО СПИСКА
@@ -79,36 +71,4 @@ const renderListMarkup = function (config) {
   searchByCountryEl.insertAdjacentHTML('beforeend', markup);
 };
 searchFieldEl.addEventListener('input', debounce(search, DEBOUNCE_DELAY));
-searchByCountryEl.addEventListener('click', debounce(showCountries, DEBOUNCE_DELAY));
-document.querySelector('form').addEventListener('submit', e => {
-  e.preventDefault();
-});
-search();
-// СТАРАЯ ВЕРСИЯ - ФУНКЦИЯ ЗАПРОСА НА СЕРВЕР ДЛЯ ПОЛУЧЕНИЯ СПИСКА СТРАН
-
-// const searchCountry = async function () {
-//     let url2 = `${BASE_URL}venues.json?&apikey=${API_KEY}`;
-//     const venues1 = await fetch(url2)
-//     const { _embedded } = await venues1.json()
-//     console.log(_embedded)
-//     const countryNames= _embedded.venues
-//     // const { _embedded: { venues } } = venuesList
-//     renderListMarkup(countryNames)
-//     // return countryNames
-// }
-
-// searchByCountryEl.addEventListener('click', debounce(searchCountry, DEBOUNCE_DELAY))
-
-// ВАРИАНТ ЗАПРОСА НА СЕРВЕР ЧЕРЕЗ ПРОМИСЫ
-
-//https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey={apikey}
-
-// const sendServerRequest = function (userQuery) {
-
-//     let url = `${BASE_URL}events.json?keyword=${userQuery}&apikey=${API_KEY}`;
-
-//     fetch(url).then(response=> response.json()).then(response=> console.log(response))
-//     // const events = await response.json()
-//     // .then(console.log(events))
-//     // renderMarkup(response)
-// }
+searchByCountryEl.addEventListener('change', debounce(showCountries, DEBOUNCE_DELAY));
