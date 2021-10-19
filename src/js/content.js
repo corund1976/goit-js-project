6fe45fb60d27e38756b84af1e49a9ed682618e10
Anna Andreikiv <andreikiv.ann@gmail.com>
2021-10-19 13:02:50 +0300

    rendering done

--- src/js/content.js
+++ src/js/content.js
@@ -1,6 +1,5 @@
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
 const API_KEY = 'kGyK62KCJILapDAPE9fz0caemViSYQAs';
-//'Q9savgjBFnHGJj2EK0EB2bfGv110AECA';
 
 const sendServerRequest = async function (userQuery) {
   let url = `${BASE_URL}events.json?keyword=${userQuery}&apikey=${API_KEY}`;
@@ -10,12 +9,8 @@ const sendServerRequest = async function (userQuery) {
 };
 
 const search = async function (e) {
-  const userQuery = 'Eurovision';
-  // sendServerRequest(userQuery);
-  // keyWord = searchFieldEl.value
-  // keyWord = 'eurov';
+  const userQuery = 'Euro';
   const reply = await sendServerRequest(userQuery);
-  //   console.log(reply);
   renderMarkup(reply);
 };
 
@@ -23,32 +18,27 @@ const renderMarkup = function (searchedEvents) {
   console.log(searchedEvents);
   let render = '';
   for (let i = 0; i < searchedEvents.page.totalElements; i++) {
-    render += `<div><ul>
-          <li><img src="${searchedEvents._embedded.events[i].images.map(img => img.url)[0]}"></li>
-          <li>Event name: ${searchedEvents._embedded.events[i].name}</li>
-          <li>Event date: ${searchedEvents._embedded.events[i].dates.start.localDate}</li>
-          <li>Event location: ${searchedEvents._embedded.events[i]._embedded.venues.map(item =>
-            item.name ? item.name : item.address?.line1,
-          )}</li>
-          </ul>
-          </div>`;
+    render += ` <li class="card__item">
+        <a href="" class="card__item__link link">
+            <div class="border-card"></div>
+            <img src="${searchedEvents._embedded.events[i].images.map(img => img.url)[0]}" 
+                alt="img with singer"
+                class="card__item__img card__item__elements">
+            <h3 class="card__item__name-of-group card__item__elements"> ${
+              searchedEvents._embedded.events[i].name
+            }</h3>
+            <p class="card__item__date-to-begin card__item__elements">${
+              searchedEvents._embedded.events[i].dates.start.localDate
+            }</p>
+            <p class="card__item__location card__item__elements"> ${searchedEvents._embedded.events[
+              i
+            ]._embedded.venues.map(item => (item.name ? item.name : item.address?.line1))}</p>
+        </a>
+    </li>`;
     console.log(searchedEvents._embedded.events.map(item => item.name));
     console.log(searchedEvents._embedded.events.map(item => item.dates.start.localDate));
-    // console.log(
-    //   searchedEvents._embedded.events.map(item =>
-    //     item._embedded.venues.map(item => (item.name === [] ? item.name : item.address.line1)),
-    //   ),
-    // );
     console.log(searchedEvents._embedded.events[i].images.map(img => img.url)[0]);
   }
-  document.querySelector('body').insertAdjacentHTML('beforeend', render);
+  document.querySelector('.card').insertAdjacentHTML('beforeend', render);
 };
 search();
-// const sendServerRequestForCountry = async function (userQuery = 'US') {
-//   let url = `${BASE_URL}events.json?countryCode=${userQuery}&apikey=${API_KEY}`;
-//   const response = await fetch(url);
-//   const events = await response.json();
-//   console.log(events);
-//   return events;
-// };
-// sendServerRequestForCountry();
