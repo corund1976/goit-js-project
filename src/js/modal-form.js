ad981de0f13f635f3006547349cb59634f40ae72
Anri <maystrenko22a@gmail.com>
2021-10-20 11:07:08 +0300

    hwf

--- src/js/modal-form.js
+++ src/js/modal-form.js
@@ -1,30 +1,47 @@
-import { sendServerRequest, search } from './search-field';
+import modalMarkupTpl from '../templates/modal-markup.hbs';
+import { search, result } from './search-field';
+// console.log('modalMarkupTpl', modalMarkupTpl);
 
 const refs = {
   eventsCardsList: document.querySelector('.events-section .card'),
   modalBtnCloseNode: document.querySelector('.backdrop .modal__closed'),
   modalNode: document.querySelector('.backdrop'),
+  bodyNode: document.querySelector('body'),
+  cardNode: document.querySelector('.card .card__item'),
 };
 
 refs.eventsCardsList.addEventListener('click', onEventClick);
-document.querySelector('body').addEventListener('keydown', onKeyPress);
+
 refs.modalBtnCloseNode.addEventListener('click', onModalClose);
 refs.modalNode.addEventListener('click', onBackdropClick);
 
-function onEventClick(e) {
+async function onEventClick(e) {
+  // if (e.target.nodeName !== 'LI') return false;
+  if (
+    e.target.nodeName !== 'IMG' &&
+    e.target.nodeName !== 'DIV' &&
+    e.target.nodeName !== 'H3' &&
+    e.target.nodeName !== 'P'
+  )
+    return false;
+  e.preventDefault();
+  refs.bodyNode.addEventListener('keydown', onKeyPress);
   refs.modalNode.classList.toggle('is-hidden');
+
+  console.log('onEventClick ~ e', e);
 }
+
 function onModalClose(e) {
+  refs.bodyNode.removeEventListener('keydown', onKeyPress);
   refs.modalNode.classList.toggle('is-hidden');
 }
 function onBackdropClick(e) {
   if (e.target === refs.modalNode) {
-    refs.modalNode.classList.toggle('is-hidden');
+    onModalClose();
   }
 }
 function onKeyPress(e) {
   if (e.code === 'Escape') {
-    refs.modalNode.classList.toggle('is-hidden');
+    onModalClose();
   }
 }
-// console.log(sendServerRequest());
---
+++ src/js/search-field.js
@@ -0,0 +1,119 @@
+import debounce from 'lodash.debounce';
+import * as config from '../config.json';
+
+const searchFieldEl = document.querySelector('#search');
+console.log('searchFieldEl', searchFieldEl);
+const searchByCountryEl = document.querySelector('#country-search');
+const DEBOUNCE_DELAY = 300;
+
+const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
+const API_KEY = 'Q9savgjBFnHGJj2EK0EB2bfGv110AECA';
+//  'kGyK62KCJILapDAPE9fz0caemViSYQAs';
+//https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=Q9savgjBFnHGJj2EK0EB2bfGv110AECA
+// ФУНКЦИЯ ПОИСКА ПО ЗАПРОСУ
+// export let userQuery = '';
+export async function search() {
+  // БЕРЁМ ДАННЫЕ ПОЛЬЗОВАТЕЛЯ
+  const userQuery = searchFieldEl.value || '';
+  // userQuery = searchFieldEl.value;
+  // ОТПРАВЛЯЕМ ЗАПРОС НА СЕРВЕР (ДАННЫЕ ПОЛЬЗОВАТЕЛЯ В ПАРАМЕТРЕ)
+  // sendServerRequest(userQuery);
+  const reply = await sendServerRequest(userQuery);
+
+  //   ВЫЗЫВАЕМ ФУНКЦИЮ ОТРИСОВКИ РАЗМЕТКИ
+  renderMarkup(reply);
+  return reply;
+}
+
+// ФУНКЦИЯ ДЛЯ ЗАПРОСОВ НА СЕРВЕР
+export const sendServerRequest = async function (userQuery) {
+  let url;
+  if (userQuery === '') {
+    url = `${BASE_URL}events.json?apikey=${API_KEY}`;
+  } else {
+    url = `${BASE_URL}events.json?keyword=${userQuery}&apikey=${API_KEY}`;
+  }
+
+  const response = await fetch(url);
+  // console.log(response.status);
+  if (response.status >= 200 && response.status < 300) {
+    const events = await response.json();
+    return events;
+  } else return Promise.reject(console.log('Requst error'));
+};
+
+const renderMarkup = function (searchedEvents) {
+  let render = '';
+  let totalEl;
+  if (searchedEvents.page.totalElements > searchedEvents.page.size) {
+    totalEl = searchedEvents.page.size;
+  } else {
+    totalEl = searchedEvents.page.totalElements;
+  }
+  for (let i = 0; i < totalEl; i++) {
+    // console.log(searchedEvents._embedded.events[i]);
+    render += ` <li class="card__item">
+            <div class="border-card"></div>
+            <img src="${searchedEvents._embedded.events[i].images[0].url}" 
+                alt="img with singer"
+                class="card__item__img card__item__elements">
+            <h3 class="card__item__name-of-group card__item__elements"> ${
+              searchedEvents._embedded.events[i].name
+            }</h3>
+            <p class="card__item__date-to-begin card__item__elements">${
+              searchedEvents._embedded.events[i].dates.start.localDate
+            }</p>
+            <p class="card__item__location card__item__elements"> ${searchedEvents._embedded.events[
+              i
+            ]._embedded.venues.map(item => (item.name ? item.name : item.address?.line1))}</p>
+    </li>`;
+  }
+  document.querySelector('.card').innerHTML = render;
+};
+
+const showCountries = function () {
+  renderListMarkup(config);
+};
+// ФУНКЦИЯ ДЛЯ РЕНДЕРИНГА РАЗМЕТКИ ВЫПАДАЮЩЕГО СПИСКА
+
+const renderListMarkup = function (config) {
+  searchByCountryEl.innerHTML = '';
+  const array = Object.entries(config);
+  const markup = array.map(item => `<option value="${item[0]}">${item[1]}</option>`).join('');
+  searchByCountryEl.insertAdjacentHTML('beforeend', markup);
+};
+searchFieldEl.addEventListener('input', debounce(search, DEBOUNCE_DELAY));
+searchByCountryEl.addEventListener('click', debounce(showCountries, DEBOUNCE_DELAY));
+document.querySelector('form').addEventListener('submit', e => {
+  e.preventDefault();
+});
+search();
+
+// СТАРАЯ ВЕРСИЯ - ФУНКЦИЯ ЗАПРОСА НА СЕРВЕР ДЛЯ ПОЛУЧЕНИЯ СПИСКА СТРАН
+
+// const searchCountry = async function () {
+//     let url2 = `${BASE_URL}venues.json?&apikey=${API_KEY}`;
+//     const venues1 = await fetch(url2)
+//     const { _embedded } = await venues1.json()
+//     console.log(_embedded)
+//     const countryNames= _embedded.venues
+//     // const { _embedded: { venues } } = venuesList
+//     renderListMarkup(countryNames)
+//     // return countryNames
+// }
+
+// searchByCountryEl.addEventListener('click', debounce(searchCountry, DEBOUNCE_DELAY))
+
+// ВАРИАНТ ЗАПРОСА НА СЕРВЕР ЧЕРЕЗ ПРОМИСЫ
+
+//https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey={apikey}
+
+// const sendServerRequest = function (userQuery) {
+
+//     let url = `${BASE_URL}events.json?keyword=${userQuery}&apikey=${API_KEY}`;
+
+//     fetch(url).then(response=> response.json()).then(response=> console.log(response))
+//     // const events = await response.json()
+//     // .then(console.log(events))
+//     // renderMarkup(response)
+// }
