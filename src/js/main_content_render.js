77f293069ebfcf063538f60059822787e30ebe3c
Anri <maystrenko22a@gmail.com>
2021-10-21 16:51:54 +0300

    fix

--- src/js/main_content_render.js
+++ src/js/main_content_render.js
@@ -3,10 +3,6 @@ export const renderMarkup = function (searchedEvents) {
   let render = '';
   let totalEl;
 
-  const animationClass = document.querySelector("#animation");
-  const animationClassName = document.querySelector("#name-marquee");
-  
-
   if (searchedEvents.page.totalElements > searchedEvents.page.size) {
     totalEl = searchedEvents.page.size;
   } else {
@@ -20,34 +16,43 @@ export const renderMarkup = function (searchedEvents) {
             <img src="${searchedEvents._embedded?.events?.[i].images.map(img => img.url)[0]}" 
                 alt="img with singer"
                 class="card__item__img card__item__elements">
-
             <h2 class="card__item__name-of-group card__item__elements card__item__animation"><span class="marquee"> ${
               searchedEvents._embedded?.events?.[i].name
-
             }</span></h2>
             <p class="card__item__date-to-begin card__item__elements">${
               searchedEvents._embedded?.events?.[i].dates.start.localDate
             }</p>
-            <a href="https://maps.google.com/?ll=${searchedEvents._embedded.events[i]._embedded?.venues.map(item =>
-            item.location.latitude)},${searchedEvents._embedded.events[i]._embedded?.venues.map(item =>
-              item.location.longitude)}" target="_blank" rel="noopener noreferrer" class="card__item__location card__item__elements card__item__animation center-location ${searchedEvents._embedded.events[i]._embedded?.venues.map(item =>
-            item.name ? item.name.length : item.address?.line1.length,) >= 20 ? "j-content" : ""}">
+            <a href="https://maps.google.com/?ll=${searchedEvents._embedded.events[
+              i
+            ]._embedded?.venues.map(
+              item => item.location.latitude,
+            )},${searchedEvents._embedded.events[i]._embedded?.venues.map(
+        item => item.location.longitude,
+      )}" target="_blank" rel="noopener noreferrer" class="card__item__location card__item__elements card__item__animation center-location ${
+        searchedEvents._embedded.events[i]._embedded?.venues.map(item =>
+          item.name ? item.name.length : item.address?.line1.length,
+        ) >= 20
+          ? 'j-content'
+          : ''
+      }">
               <span class="icon__location-bg">
             <svg class="icon__location">
                 <use href="${spriteSvg}#icon-location"></use>
               </svg>
               </span>
-             <span id="animation" ${searchedEvents._embedded.events[i]._embedded?.venues.map(item =>
-              item.name ? item.name.length : item.address?.line1.length,) > 20 ? "class='marquee-location'" : ""}>
-
+             <span id="animation" ${
+               searchedEvents._embedded.events[i]._embedded?.venues.map(item =>
+                 item.name ? item.name.length : item.address?.line1.length,
+               ) > 20
+                 ? "class='marquee-location'"
+                 : ''
+             }>
               ${searchedEvents._embedded?.events?.[i]._embedded?.venues.map(item =>
-
                 item.name ? item.name : item.address?.line1,
-              )}</span></a>
+              )}</span>
+          </a>
     </li>`;
     }
   }
   document.querySelector('.card').innerHTML = render;
 };
-
-// <img class="card__item__location-img" src="../images/card/location.png" alt="location"> ${searchedEvents._embedded.events[
