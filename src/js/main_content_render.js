d3094cf53a781b69859e21c8e00b15c32c9bdd50
Sergii Budak <corund1976@gmail.com>
2021-10-21 22:11:19 +0300

    22.11

--- src/js/main_content_render.js
+++ src/js/main_content_render.js
@@ -17,17 +17,16 @@ export const renderMarkup = function (searchedEvents) {
             <img src="${searchedEvents._embedded?.events?.[i].images.map(img => img.url)[0]}" 
                 alt="img with singer"
                 class="card__item__img card__item__elements">
-<
             <h2 class="card__item__name-of-group card__item__elements card__item__animation  ${
               searchedEvents._embedded?.events?.[i].name.length > 20 ? 'j-content' : ''
-            }"><span class="marquee"> 
-            ${searchedEvents._embedded?.events?.[i].name}</span></h2>
+              }"><span class=${
+              searchedEvents._embedded?.events?.[i].name.length > 20 ? 'marquee' : ''
+              }> 
+              ${searchedEvents._embedded?.events?.[i].name}</span></h2>
             <p class="card__item__date-to-begin card__item__elements">${
               searchedEvents._embedded?.events?.[i].dates.start.localDate
             }</p>
-            <a href="https://maps.google.com/?ll=${searchedEvents._embedded.events[
-              i
-            ]._embedded?.venues.map(
+            <a href="https://maps.google.com/?ll=${searchedEvents._embedded.events[i]._embedded?.venues.map(
               item => item.location?.latitude,
             )},${searchedEvents._embedded.events[i]._embedded?.venues.map(
         item => item.location?.longitude,
