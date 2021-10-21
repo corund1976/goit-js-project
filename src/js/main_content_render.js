7679e2dcb6a2d08656791a0969fb885824877a36
Subm1s <82820375+Subm1s@users.noreply.github.com>
2021-10-21 09:08:31 +0300

    new

--- src/js/main_content_render.js
+++ src/js/main_content_render.js
@@ -15,13 +15,13 @@ export const renderMarkup = function (searchedEvents) {
             <img src="${searchedEvents._embedded.events[i].images.map(img => img.url)[0]}" 
                 alt="img with singer"
                 class="card__item__img card__item__elements">
-            <h2 class="card__item__name-of-group card__item__elements"><span class="marquee"> ${
+            <h2 class="card__item__name-of-group card__item__elements card__item__animation"><span class="marquee"> ${
               searchedEvents._embedded.events[i].name
             }</span></h2>
             <p class="card__item__date-to-begin card__item__elements">${
               searchedEvents._embedded.events[i].dates.start.localDate
             }</p>
-            <p class="card__item__location card__item__elements">
+            <a href="https://www.google.com/maps" target="_blank" rel="noopener noreferrer" class="card__item__location card__item__elements card__item__animation">
             <span class="marquee-location">
               <svg class="icon__location">
                 <use href="${spriteSvg}#icon-location"></use>
@@ -29,7 +29,7 @@ export const renderMarkup = function (searchedEvents) {
 
               ${searchedEvents._embedded.events[i]._embedded.venues.map(item =>
                 item.name ? item.name : item.address?.line1,
-            )}</span></p>
+            )}</span></a>
     </li>`;
   }
 
