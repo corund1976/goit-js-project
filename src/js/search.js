a00c1d88040da0a2c0fea2badf66f59431a8cb4f
Anri <maystrenko22a@gmail.com>
2021-10-21 17:50:41 +0300

    replace changes

--- src/js/search.js
+++ src/js/search.js
@@ -8,20 +8,13 @@ import { renderPagination } from './pagination_render';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const searchFieldEl = document.querySelector('#search');
 const searchByCountryEl = document.querySelector('#country-search-input');
-<<<<<<< HEAD
-const DEBOUNCE_DELAY = 300;
 
-// ФУНКЦИЯ ПОИСКА ПО ЗАПРОСУ
-=======
-
->>>>>>> test
 export let country;
 export let userQuery;
 export const search = async function () {
   // БЕРЁМ ДАННЫЕ ПОЛЬЗОВАТЕЛЯ
   userQuery = searchFieldEl.value || '';
   // ОТПРАВЛЯЕМ ЗАПРОС НА СЕРВЕР (ДАННЫЕ ПОЛЬЗОВАТЕЛЯ В ПАРАМЕТРЕ)
-  // sendServerRequest(userQuery, country);
   try {
     const reply = await sendServerRequest(userQuery, country);
     renderMarkup(reply);
@@ -30,32 +23,17 @@ export const search = async function () {
     console.log(error);
     Notify.failure('Bad request');
   }
-  //   ВЫЗЫВАЕМ ФУНКЦИЮ ОТРИСОВКИ РАЗМЕТКИ
-
-  // return reply;
 };
-<<<<<<< HEAD
-
-searchFieldEl.addEventListener('input', debounce(search, DEBOUNCE_DELAY));
-// searchByCountryEl.addEventListener('click', debounce(showCountries, DEBOUNCE_DELAY));
-=======
 searchFieldEl.addEventListener('input', debounce(search, config_js.DEBOUNCE_DELAY));
->>>>>>> test
 renderListMarkup(config);
-
 searchByCountryEl.addEventListener('change', e => {
   country = document.querySelector(`#country-search option[value="${e.target.value}"]`).textContent;
   search();
 });
-
 searchByCountryEl.addEventListener('click', e => {
   searchByCountryEl.value = '';
 });
-
 document.querySelector('form').addEventListener('submit', e => {
   e.preventDefault();
 });
-
 search();
-
-
